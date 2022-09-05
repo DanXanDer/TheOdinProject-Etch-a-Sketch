c7f29eb86a012fccfe8e87bef2f2368033ff0b5c
DanXanDerGC <danielsmart5@hotmail.com>
2022-09-05 14:57:29 -0500

    Project

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const container = document.querySelector(".container");
-for(let i=0; i<16; i++){
-    const gridContainer = document.createElement("div");
-    gridContainer.style.display = "flex";
-    for(let j=0; j<16; j++){
-        const gridElement = document.createElement("div");
-        gridElement.style.height = "16px";
-        gridElement.style.width = "16px";
-        gridElement.style.borderStyle = "groove";
-        gridElement.style.borderWidth = "1px";
-        gridElement.style.borderColor = "black";
-        gridContainer.append(gridElement);
-        
+const cont = document.createElement("div");
+container.classList.add("container");
+container.style.width = "600px";
+container.style.height = "600px";
+const gridSize = document.querySelector("#sizeRange");
+const dimNumber = document.querySelector(".dimNumber");
+
+function createGrid(e) {
+    container.replaceChildren();
+    for (let i = 0; i < this.value; i++) {
+        const gridContainer = document.createElement("div");
+        gridContainer.classList.add("gridContainer");
+        gridContainer.style.display = "flex";
+        for (let j = 0; j < this.value; j++) {
+            const gridElement = document.createElement("div");
+            gridElement.style.height = pixelSize(this.value);
+            gridElement.style.width = pixelSize(this.value);
+            gridElement.classList.add("gridElement");
+            gridContainer.append(gridElement);
+        }
+        container.append(gridContainer);
     }
-    container.append(gridContainer);
+    const gridContainerArray = Array.from(document.querySelectorAll(".gridContainer"));    
+    gridContainerArray.forEach(gridContainer => {
+        gridElementArray = Array.from(document.querySelectorAll(".gridElement"));
+        gridElementArray.forEach(gridElement => gridElement.addEventListener("mouseover", colorGrid));
+    })
 }
+
+function pixelSize(gridSize) {
+    let pixSize = 600 / (gridSize);   
+    return `${pixSize}px`;
+}
+
+
+function colorGrid(e) {
+    this.classList.add("gridChangeColor");
+}
+
+function showDimensionNumber(e){
+    dimNumber.textContent = `${this.value} x ${this.value}`;
+}
+gridSize.addEventListener("mousemove", showDimensionNumber);
+gridSize.addEventListener("mouseup", createGrid);
